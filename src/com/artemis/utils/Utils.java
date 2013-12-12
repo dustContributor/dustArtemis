c2a90735ba61e7eadf19323c076ab551e3e9ef61
Admin <todust_2011@hotmail.com>
2013-12-12 08:40:08 -0300

    Removed unused import.

--- src/com/artemis/utils/Utils.java
+++ src/com/artemis/utils/Utils.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 
 public class Utils {
