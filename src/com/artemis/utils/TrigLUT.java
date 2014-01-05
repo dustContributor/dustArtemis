289a5e3612beded4b6bc1da437e163ef5f12cfcc
TheChubu <TheChubu@localhost>
2014-01-05 10:12:51 -0300

    Removed a few statics that weren't used nor visible beyond the class. Added 4 exact cardinal points according to original Java-Gaming forum thread.

--- src/com/artemis/utils/TrigLUT.java
+++ src/com/artemis/utils/TrigLUT.java
@@ -2,54 +2,66 @@ package com.artemis.utils;
 
 // Thanks to Riven
 // From: http://riven8192.blogspot.com/2009/08/fastmath-sincos-lookup-tables.html
-public class TrigLUT {
-	public static void main(String[] args) {
-		System.out.println(cos((float) Math.PI));
-		System.out.println(cosDeg(180f));
+public class TrigLUT
+{
+	public static final float sin ( final float rad )
+	{
+		return sin[(int) ( rad * radToIndex ) & SIN_MASK];
 	}
 
-	public static final float sin(float rad) {
-		return sin[(int) (rad * radToIndex) & SIN_MASK];
+	public static final float cos ( final float rad )
+	{
+		return cos[(int) ( rad * radToIndex ) & SIN_MASK];
 	}
 
-	public static final float cos(float rad) {
-		return cos[(int) (rad * radToIndex) & SIN_MASK];
+	public static final float sinDeg ( final float deg )
+	{
+		return sin[(int) ( deg * degToIndex ) & SIN_MASK];
 	}
 
-	public static final float sinDeg(float deg) {
-		return sin[(int) (deg * degToIndex) & SIN_MASK];
-	}
-
-	public static final float cosDeg(float deg) {
-		return cos[(int) (deg * degToIndex) & SIN_MASK];
+	public static final float cosDeg ( final float deg )
+	{
+		return cos[(int) ( deg * degToIndex ) & SIN_MASK];
 	}
 
 	// Not used.
 //	private static final float RAD, DEG;
-	private static final int SIN_BITS, SIN_MASK, SIN_COUNT;
-	private static final float radFull, radToIndex;
-	private static final float degFull, degToIndex;
+	private static final int SIN_MASK;
+	private static final float radToIndex;
+	private static final float degToIndex;
 	private static final float[] sin, cos;
 
-	static {
-//		RAD = (float) Math.PI / 180.0f;
-//		DEG = 180.0f / (float) Math.PI;
+	static 
+	{
+		// RAD = (float) Math.PI / 180.0f;
+		// DEG = 180.0f / (float) Math.PI;
 
-		SIN_BITS = 12;
-		SIN_MASK = ~(-1 << SIN_BITS);
-		SIN_COUNT = SIN_MASK + 1;
+		final int SIN_BITS = 12;
+		
+		SIN_MASK = ~ ( -1 << SIN_BITS );
+		
+		final int SIN_COUNT = SIN_MASK + 1;
 
-		radFull = (float) (Math.PI * 2.0);
-		degFull = (float) (360.0);
+		final float radFull = (float) ( Math.PI * 2.0 );
+		final float degFull = 360.0f;
+		
 		radToIndex = SIN_COUNT / radFull;
 		degToIndex = SIN_COUNT / degFull;
 
 		sin = new float[SIN_COUNT];
 		cos = new float[SIN_COUNT];
 
-		for (int i = 0; i < SIN_COUNT; i++) {
-			sin[i] = (float) Math.sin((i + 0.5f) / SIN_COUNT * radFull);
-			cos[i] = (float) Math.cos((i + 0.5f) / SIN_COUNT * radFull);
+		for ( int i = 0; i < SIN_COUNT; ++i )
+		{
+			sin[i] = (float) Math.sin( ( i + 0.5f ) / SIN_COUNT * radFull );
+			cos[i] = (float) Math.cos( ( i + 0.5f ) / SIN_COUNT * radFull );
+		}
+
+		// Four cardinal directions (credits: Nate)
+		for ( int i = 0; i < 360; i += 90 )
+		{
+			sin[(int) ( i * degToIndex ) & SIN_MASK] = (float) Math.sin( i * Math.PI / 180.0 );
+			cos[(int) ( i * degToIndex ) & SIN_MASK] = (float) Math.cos( i * Math.PI / 180.0 );
 		}
 	}
 }
