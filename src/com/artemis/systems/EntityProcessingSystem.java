f05e7885703a2c885114cd5fd789113639918478
TheChubu <TheChubu@localhost>
2014-01-13 19:47:29 -0300

    Formatted it, added finals.

--- src/com/artemis/systems/EntityProcessingSystem.java
+++ src/com/artemis/systems/EntityProcessingSystem.java
@@ -12,28 +12,35 @@ import com.artemis.utils.ImmutableBag;
  * @author Arni Arent
  *
  */
-public abstract class EntityProcessingSystem extends EntitySystem {
-	
-	public EntityProcessingSystem(Aspect aspect) {
-		super(aspect);
+public abstract class EntityProcessingSystem extends EntitySystem
+{
+
+	public EntityProcessingSystem ( final Aspect aspect )
+	{
+		super( aspect );
 	}
 
 	/**
 	 * Process a entity this system is interested in.
-	 * @param e the entity to process.
+	 * 
+	 * @param e
+	 *            the entity to process.
 	 */
-	protected abstract void process(Entity e);
+	protected abstract void process ( final Entity e );
 
 	@Override
-	protected final void processEntities(ImmutableBag<Entity> entities) {
-		for (int i = 0, s = entities.size(); s > i; i++) {
-			process(entities.get(i));
+	protected final void processEntities ( final ImmutableBag<Entity> entities )
+	{
+		for ( int i = 0; i < entities.size(); ++i )
+		{
+			process( entities.get( i ) );
 		}
 	}
-	
+
 	@Override
-	protected boolean checkProcessing() {
+	protected boolean checkProcessing ()
+	{
 		return true;
 	}
-	
+
 }
