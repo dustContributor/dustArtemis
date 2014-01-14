edad4c79590f9783affd9c59cbf0618dbc1cd5ca
TheChubu <TheChubu@localhost>
2014-01-14 15:29:58 -0300

    Tidying up a bit the class. Made final wherever possible mostly.

--- src/com/artemis/EntitySystem.java
+++ src/com/artemis/EntitySystem.java
@@ -14,35 +14,39 @@ import com.artemis.utils.ImmutableBag;
  * @author Arni Arent
  *
  */
-public abstract class EntitySystem implements EntityObserver {
+public abstract class EntitySystem implements EntityObserver
+{
 	private final int systemIndex;
 
 	protected World world;
 
-	private Bag<Entity> actives;
+	private final Bag<Entity> actives;
 
+	// Commented out since it wasn't used.
 //	private Aspect aspect;
 
-	private BitSet allSet;
-	private BitSet exclusionSet;
-	private BitSet oneSet;
+	private final BitSet allSet;
+	private final BitSet exclusionSet;
+	private final BitSet oneSet;
 
 	private boolean passive;
 
-	private boolean dummy;
+	private final boolean dummy;
 	
 	/**
 	 * Creates an entity system that uses the specified aspect as a matcher against entities.
 	 * @param aspect to match against entities
 	 */
-	public EntitySystem(Aspect aspect) {
+	public EntitySystem ( Aspect aspect )
+	{
 		actives = new Bag<>();
-//		this.aspect = aspect;
+		// this.aspect = aspect;
 		allSet = aspect.getAllSet();
 		exclusionSet = aspect.getExclusionSet();
 		oneSet = aspect.getOneSet();
-		systemIndex = SystemIndexManager.getIndexFor(this.getClass());
-		dummy = allSet.isEmpty() && oneSet.isEmpty(); // This system can't possibly be interested in any entity, so it must be "dummy"
+		systemIndex = SystemIndexManager.getIndexFor( this.getClass() );
+		// This system can't possibly be interested in any entity, so it must be "dummy"
+		dummy = allSet.isEmpty() && oneSet.isEmpty(); 
 	}
 	
 	/**
@@ -53,10 +57,12 @@ public abstract class EntitySystem implements EntityObserver {
 		// Empty method.
 	}
 
-	public final void process() {
-		if(checkProcessing()) {
+	public final void process ()
+	{
+		if ( checkProcessing() )
+		{
 			begin();
-			processEntities(actives);
+			processEntities( actives );
 			end();
 		}
 	}
@@ -113,123 +119,148 @@ public abstract class EntitySystem implements EntityObserver {
 	 * Will check if the entity is of interest to this system.
 	 * @param e entity to check
 	 */
-	protected final void check(Entity e) {
-		if(dummy) {
+	protected final void check ( final Entity e )
+	{
+		if ( dummy )
+		{
 			return;
 		}
-		
-		boolean contains = e.getSystemBits().get(systemIndex);
-		boolean interested = true; // possibly interested, let's try to prove it wrong.
-		
-		BitSet componentBits = e.getComponentBits();
 
-		// Check if the entity possesses ALL of the components defined in the aspect.
-		if(!allSet.isEmpty()) {
-			for (int i = allSet.nextSetBit(0); i >= 0; i = allSet.nextSetBit(i+1)) {
-				if(!componentBits.get(i)) {
+		final BitSet componentBits = e.getComponentBits();
+		
+		boolean interested = true; // possibly interested, let's try to prove it wrong.
+
+		// Check if the entity possesses ALL of the components defined in the
+		// aspect.
+		if ( !allSet.isEmpty() )
+		{
+			for ( int i = allSet.nextSetBit( 0 ); i >= 0; i = allSet.nextSetBit( i + 1 ) )
+			{
+				if ( !componentBits.get( i ) )
+				{
 					interested = false;
 					break;
 				}
 			}
 		}
 		
-		// Check if the entity possesses ANY of the exclusion components, if it does then the system is not interested.
-		if(!exclusionSet.isEmpty() && interested) {
-			interested = !exclusionSet.intersects(componentBits);
+		// Check if the entity possesses ANY of the exclusion components, if it
+		// does then the system is not interested.
+		if ( !exclusionSet.isEmpty() && interested )
+		{
+			interested = !exclusionSet.intersects( componentBits );
 		}
+
+		// Check if the entity possesses ANY of the components in the oneSet. If
+		// so, the system is interested.
+		if ( !oneSet.isEmpty() )
+		{
+			interested = oneSet.intersects( componentBits );
+		}
+
+		final boolean contains = e.getSystemBits().get( systemIndex );
 		
-		// Check if the entity possesses ANY of the components in the oneSet. If so, the system is interested.
-		if(!oneSet.isEmpty()) {
-			interested = oneSet.intersects(componentBits);
+		if ( interested && !contains )
+		{
+			insertToSystem( e );
 		}
-
-		if (interested && !contains) {
-			insertToSystem(e);
-		} else if (!interested && contains) {
-			removeFromSystem(e);
+		else if ( !interested && contains )
+		{
+			removeFromSystem( e );
 		}
 	}
 
-	private void removeFromSystem(Entity e) {
-		actives.remove(e);
-		e.getSystemBits().clear(systemIndex);
-		removed(e);
+	private void removeFromSystem ( final Entity e )
+	{
+		actives.remove( e );
+		e.getSystemBits().clear( systemIndex );
+		removed( e );
 	}
 
-	private void insertToSystem(Entity e) {
-		actives.add(e);
-		e.getSystemBits().set(systemIndex);
-		inserted(e);
+	private void insertToSystem ( final Entity e )
+	{
+		actives.add( e );
+		e.getSystemBits().set( systemIndex );
+		inserted( e );
 	}
-	
-	
-	@Override
-	public final void added(Entity e) {
-		check(e);
-	}
-	
-	@Override
-	public final void changed(Entity e) {
-		check(e);
-	}
-	
-	@Override
-	public final void deleted(Entity e) {
-		if(e.getSystemBits().get(systemIndex)) {
-			removeFromSystem(e);
-		}
-	}
-	
-	@Override
-	public final void disabled(Entity e) {
-		if(e.getSystemBits().get(systemIndex)) {
-			removeFromSystem(e);
-		}
-	}
-	
-	@Override
-	public final void enabled(Entity e) {
-		check(e);
-	}
-	
 
-	protected final void setWorld(World world) {
+	@Override
+	public final void added ( Entity e )
+	{
+		check( e );
+	}
+
+	@Override
+	public final void changed ( Entity e )
+	{
+		check( e );
+	}
+
+	@Override
+	public final void deleted ( Entity e )
+	{
+		if ( e.getSystemBits().get( systemIndex ) )
+		{
+			removeFromSystem( e );
+		}
+	}
+
+	@Override
+	public final void disabled ( Entity e )
+	{
+		if ( e.getSystemBits().get( systemIndex ) )
+		{
+			removeFromSystem( e );
+		}
+	}
+
+	@Override
+	public final void enabled ( Entity e )
+	{
+		check( e );
+	}
+
+	protected final void setWorld ( World world )
+	{
 		this.world = world;
 	}
-	
-	protected boolean isPassive() {
+
+	protected boolean isPassive ()
+	{
 		return passive;
 	}
 
-	protected void setPassive(boolean passive) {
+	protected void setPassive ( boolean passive )
+	{
 		this.passive = passive;
 	}
-	
-	public ImmutableBag<Entity> getActives() {
+
+	public ImmutableBag<Entity> getActives ()
+	{
 		return actives;
 	}
 	
 	
 	
 	/**
-	 * Used to generate a unique bit for each system.
+	 * Used to generate a unique bit for each system. 
 	 * Only used internally in EntitySystem.
 	 */
-	private static class SystemIndexManager
+	private static final class SystemIndexManager
 	{
 		private static int INDEX = 0;
 		private static final HashMap<Class<? extends EntitySystem>, Integer> indices = new HashMap<>();
 
-		static int getIndexFor ( Class<? extends EntitySystem> es )
+		static final int getIndexFor ( final Class<? extends EntitySystem> es )
 		{
 			Integer index = indices.get( es );
-			
+
 			if ( index == null )
 			{
 				index = Integer.valueOf( INDEX++ );
 				indices.put( es, index );
 			}
-			
+
 			return index.intValue();
 		}
 	}
