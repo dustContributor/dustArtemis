faec71539f61ac255e11745ca91cd3e6fe5c31f2
TheChubu <todust_2011@hotmail.com>
2014-01-29 05:41:06 -0300

    Added two EntityProcessingSystems that use parallel streams by default.

---
+++ src/com/artemis/systems/ParallelEntityProcessingSystem.java
@@ -0,0 +1,54 @@
+package com.artemis.systems;
+
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import com.artemis.Aspect;
+import com.artemis.Entity;
+import com.artemis.EntitySystem;
+import com.artemis.utils.Bag;
+import com.artemis.utils.ImmutableBag;
+
+/**
+ * A parallel EntitySystem implemented with Java 8 parallelStream().
+ * 
+ * @author The Chubu
+ *
+ */
+public abstract class ParallelEntityProcessingSystem extends EntitySystem
+{
+
+	public ParallelEntityProcessingSystem ( final Aspect aspect )
+	{
+		super( aspect );
+	}
+
+	/**
+	 * Process a entity this system is interested in.
+	 * 
+	 * @param e
+	 *            the entity to process.
+	 */
+	protected abstract void process ( final Entity e );
+	
+	@Override
+	protected void processEntities ( final ImmutableBag<Entity> entities )
+	{
+		final Bag<Entity> bag = (Bag<Entity>) entities;
+		
+		final Spliterator<Entity> split = Spliterators.spliterator( bag.getData(), Spliterator.IMMUTABLE );
+		
+		final Stream<Entity> stream = StreamSupport.stream( split, true );
+		
+		stream.limit( bag.size() ).forEach( e -> process( e ) );
+	}
+
+	@Override
+	protected boolean checkProcessing ()
+	{
+		return true;
+	}
+
+}
