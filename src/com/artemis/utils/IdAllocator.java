fc1041fd8291bfd4fcfb8cb1e0bea8c5e037b0e3
dustContributor <todust_2011@hotmail.com>
2015-02-13 15:28:40 -0300

    - Now IdAllocator uses IntBag instead of a Bag of FreeRange objects, that way less garbage is created and hopefully less indirections too.

--- src/com/artemis/utils/IdAllocator.java
+++ src/com/artemis/utils/IdAllocator.java
@@ -12,7 +12,7 @@ import static java.lang.Integer.MAX_VALUE;
 public final class IdAllocator
 {
 	/** List of ranges of free ID numbers. */
-	private final Bag<FreeRange> freeRanges;
+	private final IntBag freeRanges;
 
 	/**
 	 * Creates an IdAllocator that will manage IDs in the interval [0,
@@ -34,8 +34,8 @@ public final class IdAllocator
 	 */
 	public IdAllocator ( int rangeStart, int rangeEnd )
 	{
-		this.freeRanges = new Bag<>( FreeRange.class, 16 );
-		this.freeRanges.addUnsafe( new FreeRange( rangeStart, rangeEnd ) );
+		this.freeRanges = new IntBag( 16 );
+		insertFreeRange( 0, rangeStart, rangeEnd );
 	}
 
 	/**
@@ -46,19 +46,15 @@ public final class IdAllocator
 	 */
 	public final int alloc ()
 	{
-		// Get lowest free range.
-		FreeRange fRange = freeRanges.getUnsafe( 0 );
+		final int[] fRanges = freeRanges.data;
 		// Free range's start will be new ID.
-		int id = fRange.start;
-		// New start will be that ID plus one.
-		int nStart = id + 1;
-		fRange.start = nStart;
+		final int id = fRanges[0]++;
 
 		// If free range's end was reached,
 		// remove it from the list.
-		if ( nStart >= fRange.end )
+		if ( fRanges[0] >= fRanges[1] )
 		{
-			freeRanges.eraseUnsafe( 0 );
+			freeRanges.eraseRangeUnsafe( 0, 2 );
 		}
 		/*
 		 * We're going to assume that you didn't ran out of IDs (ie, that
@@ -78,13 +74,12 @@ public final class IdAllocator
 		 * We're going to assume you're not freeing an ID thats outside of this
 		 * IdAllocator's initial range.
 		 */
-		int frSize = freeRanges.size();
-		FreeRange[] fRanges = freeRanges.data();
+		final int frSize = freeRanges.size();
+		final int[] fRanges = freeRanges.data();
 
-		for ( int i = 0; i < frSize; ++i )
+		for ( int i = 0; i < frSize; i += 2 )
 		{
-			FreeRange fRange = fRanges[i];
-			int frStart = fRange.start;
+			int frStart = fRanges[i];
 			// If ID is to the left.
 			if ( frStart > id )
 			{
@@ -92,20 +87,17 @@ public final class IdAllocator
 				if ( frStart == (id + 1) )
 				{
 					// Set new free range start as ID.
-					fRange.start = id;
+					fRanges[i] = id;
 					// If it isn't the first range, update range on the left.
 					if ( i != 0 )
 					{
-						// Grab range on the left.
-						FreeRange lfRange = fRanges[i - 1];
-						int lfrEnd = lfRange.end;
 						// If range on the left ends at ID.
-						if ( lfrEnd == id )
+						if ( fRanges[i - 1] == id )
 						{
 							// Extend left range limit to right range limit.
-							lfRange.end = fRange.end;
+							fRanges[i - 1] = fRanges[i + 1];
 							// Remove right range.
-							freeRanges.eraseUnsafe( i );
+							freeRanges.eraseRangeUnsafe( i, 2 );
 						}
 					}
 					return;
@@ -113,13 +105,11 @@ public final class IdAllocator
 				// If ID isn't next to free range.
 				if ( i != 0 )
 				{
-					// Grab range to the left.
-					FreeRange lfRange = fRanges[i - 1];
 					// If left range ends at ID.
-					if ( lfRange.end == id )
+					if ( fRanges[i - 1] == id )
 					{
-						// Update left range's end and return.
-						lfRange.end = id + 1;
+						// Extend left range's end and return.
+						fRanges[i - 1] = id + 1;
 						return;
 					}
 				}
@@ -127,16 +117,28 @@ public final class IdAllocator
 				 * No adjacent free range was found for given ID, make a new
 				 * one.
 				 */
-				freeRanges.ensureCapacity( frSize + 1 );
-				freeRanges.insertUnsafe( i, new FreeRange( id, id + 1 ) );
+				insertFreeRange( i, id, id + 1 );
 				return;
 			}
 		}
 
 	}
 
+	private final void insertFreeRange ( final int index, final int start, final int end )
+	{
+		final int newSize = freeRanges.size() + 2;
+		freeRanges.ensureCapacity( newSize );
+		freeRanges.setSize( newSize );
+		final int[] fRanges = freeRanges.data();
+		// Shift to the right.
+		System.arraycopy( fRanges, index, fRanges, index + 2, newSize - index );
+		// Store free range.
+		fRanges[index] = start;
+		fRanges[index + 1] = end;
+	}
+
 	@Override
-	public String toString ()
+	public final String toString ()
 	{
 		StringBuilder sb = new StringBuilder( 512 );
 		sb.append( "ID ALLOCATOR: " );
@@ -144,32 +146,15 @@ public final class IdAllocator
 		sb.append( System.lineSeparator() );
 		sb.append( "FREE RANGES: " );
 
+		int[] fRanges = freeRanges.data();
+
 		for ( int i = 0; i < freeRanges.size(); ++i )
 		{
 			sb.append( System.lineSeparator() );
-			sb.append( freeRanges.getUnsafe( i ).toString() );
+			sb.append( "START: " + fRanges[i] + " - END: " + fRanges[i + 1] );
 		}
 
 		return sb.toString();
 	}
 
-	private static final class FreeRange
-	{
-		/** Start of the free ID range. Inclusive. */
-		int start;
-		/** End of the free ID range. Exclusive. */
-		int end;
-
-		FreeRange ( int start, int end )
-		{
-			this.start = start;
-			this.end = end;
-		}
-
-		@Override
-		public String toString ()
-		{
-			return super.toString() + " START: " + start + " - END: " + end;
-		}
-	}
 }
