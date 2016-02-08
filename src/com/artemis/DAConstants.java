e85a83b6d60daa71fc76abf1a2b6a4427c4cc027
dustContributor <todust_2011@hotmail.com>
2016-02-08 14:34:31 -0300

    - Reformatted the file, made the cfg path public.

--- src/com/artemis/DAConstants.java
+++ src/com/artemis/DAConstants.java
@@ -8,42 +8,58 @@ import java.util.Properties;
 /**
  * dustArtemis constants used through the framework.
  * 
- * <br> <p> These values are loaded from a config file when the class is loaded.
- * Config file path is retrieved from {@value #CFG_FILE_PROPERTY_NAME} property
- * in Java's system properties. </p> </br>
+ * <p>
+ * These values are loaded from a config file when the class is loaded. Config
+ * file path is retrieved from {@value #CFG_FILE_PROPERTY_NAME} property in
+ * Java's system properties.
+ * </p>
  * 
- * <br><p> Constants not specified in the file will be set to a reasonable
- * default, also all constants have minimum bounds too. </p> </br>
+ * <p>
+ * Constants not specified in the file will be set to a reasonable default, also
+ * all constants have minimum bounds too.
+ * </p>
  * 
- * <br><p> Remember that most of these values aren't limits, they're
- * approximates. If you add more entities than you specified, backing
- * collections will grow and dustArtemis keeps working. These values are useful
- * mostly to specify default values so the backing collections don't trash
- * arrays at application startup when adding components/entities to your World
- * instance. </p> </br>
+ * <p>
+ * Remember that most of these values aren't limits, they're approximates. If
+ * you add more entities than you specified, backing collections will grow and
+ * dustArtemis keeps working. These values are useful mostly to specify default
+ * values so the backing collections don't trash arrays at application startup
+ * when adding components/entities to your World instance.
+ * </p>
  * 
- * <br><p> In this regard, {@link #COMPONENT_TYPES_COUNT} is actually a limit.
- * Since the introduction of fixed-size bit sets (which are much more cost
- * efficient), dustArtemis needs to know how many component types there will be
- * around to decide the size of the bit sets that it will use, so have that in
- * mind too. </br></p>
+ * <p>
+ * In this regard, {@link #COMPONENT_TYPES_COUNT} is actually a limit. Since the
+ * introduction of fixed-size bit sets (which are much more cost efficient),
+ * dustArtemis needs to know how many component types there will be around to
+ * decide the size of the bit sets that it will use, so have that in mind too.
+ * </p>
  * 
- * <br><p> For example, to set the Bag constants, you'd need to put this in a
- * plain text file: </p></br>
+ * <p>
+ * For example, to set the Bag constants, you'd need to put this in a plain text
+ * file:
+ * </p>
  * 
- * <p><code><br>BAG_DEFAULT_CAPACITY=32</br>
- * <br>BAG_GROW_RATE_THRESOLD=1024</br></code></p>
+ * <pre>
+ * BAG_DEFAULT_CAPACITY=32
+ * BAG_GROW_RATE_THRESOLD=1024
+ * </pre>
  * 
- * <br><p> Then just point to its relative path (from the application's POV)
- * where the file is located like this:</p></br>
+ * <p>
+ * Then just point to its relative path (from the application's POV) where the
+ * file is located like this:
+ * </p>
  * 
- * <p><code><br>System.getProperties().put( {@value #CFG_FILE_PROPERTY_NAME},
- * "cfg/artemis.cfg");</br></code></p>
+ * <pre>
+ * System.getProperties().put( {@value #CFG_FILE_PROPERTY_NAME},
+ * "cfg/artemis.cfg");
+ * </pre>
  * 
- * <br><p> Extension is not important, it just needs to be a plain text file.
- * Since this uses Java Properties API, all {@link java.util.Properties}
- * particulars apply. You can always check dustArtemis sources if you need to
- * know something specific about it. </p></br>
+ * <p>
+ * Extension is not important, it just needs to be a plain text file. Since this
+ * uses Java Properties API, all {@link java.util.Properties} particulars apply.
+ * You can always check dustArtemis sources if you need to know something
+ * specific about it.
+ * </p>
  * 
  * @author dustContributor
  *
@@ -57,7 +73,7 @@ public final class DAConstants
 	}
 
 	/** Name of the property that will be used to fetch config file path from. */
-	private static final String CFG_FILE_PROPERTY_NAME = "dustArtemis.cfgpath";
+	public static final String CFG_FILE_PROPERTY_NAME = "dustArtemis.cfgpath";
 
 	/** Default capacity for ImmutableBag and subclasses. */
 	public static final int BAG_DEFAULT_CAPACITY;
@@ -68,8 +84,8 @@ public final class DAConstants
 	/** Approximate amount of entities active in each system. */
 	public static final int APPROX_ENTITIES_PER_SYSTEM;
 	/**
-	 * Count of the different component types there will be. This value is used
-	 * so dustArtemis knows the size of the bit set it will use for initializing
+	 * Count of the different component types there will be. This value is used so
+	 * dustArtemis knows the size of the bit set it will use for initializing
 	 * entities and aspects. If you add more component types than you specify
 	 * here, things will probably break.
 	 */
@@ -106,55 +122,54 @@ public final class DAConstants
 	}
 
 	/**
-	 * Returns an int fetched and parsed from the properties table, or defaults
-	 * to passed value if there isn't one or the value couldn't be parsed.
+	 * Returns an int fetched and parsed from the properties table, or defaults to
+	 * passed value if there isn't one or the value couldn't be parsed.
 	 */
 	private static final int getIntOrDefault ( Properties props, String key, int defValue )
 	{
-		try
+		final String tmp = props.getProperty( key );
+		if ( tmp != null )
 		{
-			String tmp = props.getProperty( key );
-			if ( tmp != null )
+			try
 			{
 				return Integer.parseInt( tmp );
 			}
-		}
-		// Prolly the only exception type that could be raised here.
-		catch ( NumberFormatException ex )
-		{
-			// Fail silently.
+			// Prolly the only exception type that could be raised here.
+			catch ( NumberFormatException ex )
+			{
+				// Fail silently.
+			}
 		}
 
 		return defValue;
 	}
 
 	/**
-	 * Returns a boolean fetched and parsed from the properties table, or
-	 * defaults to passed value if there isn't one or the value couldn't be
-	 * parsed.
+	 * Returns a boolean fetched and parsed from the properties table, or defaults
+	 * to passed value if there isn't one or the value couldn't be parsed.
 	 */
 	private static final boolean getBoolOrDefault ( Properties props, String key, boolean defValue )
 	{
-		try
+		final String tmp = props.getProperty( key );
+		if ( tmp != null )
 		{
-			String tmp = props.getProperty( key );
-			if ( tmp != null )
+			try
 			{
 				return Boolean.parseBoolean( tmp );
 			}
-		}
-		// Prolly the only exception type that could be raised here.
-		catch ( NumberFormatException ex )
-		{
-			// Fail silently.
+			// Prolly the only exception type that could be raised here.
+			catch ( NumberFormatException ex )
+			{
+				// Fail silently.
+			}
 		}
 
 		return defValue;
 	}
 
 	/**
-	 * Loads configuration file for dustArtemis constants, fetching the path
-	 * from the property {@value #CFG_FILE_PROPERTY_NAME}.
+	 * Loads configuration file for dustArtemis constants, fetching the path from
+	 * the property {@value #CFG_FILE_PROPERTY_NAME}.
 	 */
 	private static final Properties loadCfgFile ()
 	{
